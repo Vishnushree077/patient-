f52a2f5d0eea625c44d3a95fc85638bf2f35dc9d
Amruth M <amruth.nimblix@outlook.com>
2026-03-24 07:53:24 +0530

    Delete HealthCareHub directory

--- HealthCareHub/src/main/java/nimblix/in/HealthCareHub/service/AdmissionService.java
+++
@@ -1,9 +0,0 @@
-package nimblix.in.HealthCareHub.service;
-
-import nimblix.in.HealthCareHub.request.AdmitPatientRequest;
-import nimblix.in.HealthCareHub.response.AdmitPatientResponse;
-
-public interface AdmissionService {
-
-    AdmitPatientResponse admitPatient(AdmitPatientRequest request);
-}
